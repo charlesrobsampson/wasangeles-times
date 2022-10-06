28c0f35ff2a8fc017b1a11e53a0fbdc1099a59d0
Charles Sampson <csampson@qualtrics.com>
2022-10-06 09:14:08 -0600

    schedule comment

--- stacks/stack.js
+++ stacks/stack.js
@@ -29,7 +29,7 @@ export function stack({ stack }) {
   new Cron(stack, "getAvyForecast", {
     // schedule: "rate(1 minute)", // for testing
     //schedule: "rate(30 minutes)", // for prod this needs to only run every 15 mins from 5 am to 8 am MST during the season Oct - June?
-		schedule: 'cron(0/15 10-15 * 10-6 ? *)',
+		schedule: 'cron(0/15 10-15 * 10-6 ? *)', // This is in UTC so 10 - 15 should convert to 4 - 9 MST I gave it an hour on each side cuz I didn't want to think about daylight savings
     job: {
       function: {
         handler: "functions/getAvyForecast.main",
